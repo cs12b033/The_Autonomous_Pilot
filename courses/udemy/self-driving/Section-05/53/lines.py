9df6572631812b720d039f4de9ce1f71b03c9a35
I341551 <ajay.pratap.singh@sap.com>
2019-12-26 02:43:57 +0530

    added under try catch

--- courses/udemy/self-driving/Section-05/53/lines.py
+++ courses/udemy/self-driving/Section-05/53/lines.py
@@ -3,8 +3,11 @@ from LaneDetector import LaneDetector
 
 
 if __name__ == '__main__':
-    ldIm = LaneDetector()
-    ldIm.detectLane("test_image.jpg")
-    ldVd = LaneDetector()
-    ldVd.detectLane("test2.mp4")
-    help(LaneDetector)
+    try:
+        ldIm = LaneDetector()
+        ldIm.detectLane("test_image.jpg")
+        ldVd = LaneDetector()
+        ldVd.detectLane("test2.mp4")
+        # help(LaneDetector)
+    except Exception as e:
+        print(e.__str__())
